7f3956f959cfed60a93e44796dbcaac106af8096
steve-369 <106300079+steve-369@users.noreply.github.com>
2022-12-15 13:18:18 +0530

    socialLite using laravel

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('auth/google', [GoogleController::class, 'redirectToGoogle']);
+Route::get('auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
+
