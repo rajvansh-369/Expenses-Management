578fe713268ae2f0b05bd18639de988f22d7c3ed
steve-369 <106300079+steve-369@users.noreply.github.com>
2022-12-20 12:30:51 +0530

    socialLite google redirect

--- config/services.php
+++ config/services.php
@@ -34,7 +34,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIEND_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://127.0.0.1:8001/admin',
+        'redirect' => env('GOOGLE_redirect'),
     ],
 
 ];
