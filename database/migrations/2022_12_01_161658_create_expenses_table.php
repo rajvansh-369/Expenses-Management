ac66db65f379265d7d5d3608137cad7b40603a30
steve-369 <106300079+steve-369@users.noreply.github.com>
2022-12-24 10:31:41 +0530

    Toggle Button Fix

--- database/migrations/2022_12_01_161658_create_expenses_table.php
+++ database/migrations/2022_12_01_161658_create_expenses_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->integer('amount');
             $table->string('description');
             $table->integer('pos_neg');
-            $table->integer('home_transaction')->default(1);
+            $table->boolean('home_transaction');
             $table->integer('is_active')->default(1);
             $table->dateTime('transaction_date');
             $table->timestamps();
