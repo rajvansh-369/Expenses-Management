2dcb60c1b974003d4b67798d7eab9fac2cb71de6
steve-369 <106300079+steve-369@users.noreply.github.com>
2023-01-12 10:46:16 +0530

    API added

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BaseController extends Controller
+{
+    
+
+    
+    public function sendResponse($result, $message)
+    {
+    	$response = [
+            'status' => true,
+            'data'    => $result,
+            'message' => $message,
+        ];
+
+
+        return response()->json($response, 200);
+    }
+
+    public function sendError($error, $errorMessages = [], $code = 404)
+    {
+    	$response = [
+            'status' => false,
+            'data'    =>[],
+            'message' => $error,
+        ];
+
+
+        if(!empty($errorMessages)){
+            $response['data'] = $errorMessages;
+        }
+
+
+        return response()->json($response, $code);
+    }
+}
