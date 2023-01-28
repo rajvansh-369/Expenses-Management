d61bd78d95fbb7ae4efe491136cd9e3ae2124e57
steve-369 <106300079+steve-369@users.noreply.github.com>
2023-01-29 00:57:08 +0530

    dashboad fixed

--- resources/views/vendor/filament/components/global-search/actions/index.blade.php
+++
@@ -1,9 +0,0 @@
-@props([
-    'actions',
-])
-
-<div {{ $attributes->class('filament-global-search-actions mt-4 flex gap-3') }}>
-    @foreach ($actions as $action)
-        {{ $action }}
-    @endforeach
-</div>
