4e47322de89c024a3e648e43e6ebbd37cd2b918b
Snehal <106300079+rajvansh-369@users.noreply.github.com>
2023-04-12 19:01:36 +0530

    Revert "portfolio added"

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\PortfolioController;
 use Illuminate\Support\Facades\Route;
 use Laravel\Socialite\Facades\Socialite;
 /*
@@ -14,12 +13,9 @@ use Laravel\Socialite\Facades\Socialite;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('/', [PortfolioController::class,'index'])->name('portfolio');
-
+Route::get('/', function () {
+    return view('welcome');
+});
 
 
 Route::get('/auth/redirect', function () {
